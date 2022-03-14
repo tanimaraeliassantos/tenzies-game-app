a8ec17d5e5c564645c5b9caba93e61c639017390
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-14 13:28:13 +1100

    Update App.js
    import useState
    Add comments to understand the logic behind each chunk of code
    Define state to start with new dice (loop 10 times over integers numbers from 1 to 6 and push the results to new array).
    Remove console.log
    Add function to rollDice at the click of the button by setting setDice from state with the allNewDice function to get a new array of values at each click
    define constant diceElements by looping over each element in the array and rendering each die as a component with its value
    Remove manual rendering of die
    Add automatic rendering of dice with diceElements
    Add button with function to be activated at click

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Die from "./Die";
 
 // Write a function that returns an array of 10 random numbers between 1-6 inclusive.
 // Log the array of numbers to the console for now.
+// Create state to hold array of numbers
+// Map over the state to generate array of Die and render those in place of the manually written elements
+// Create a Roll Dice button that will re-roll all 10 dice
+// Clicking the button should generate an array of numbers and set the dice state to that new array
+// Re-rendering the array to the page
 
 export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  // new array to hold numbers
+  // loop 10 times
+  // push a random number from 1-6 to my array
+  // return array
+
   function allNewDice() {
-    // new array to hold numbers
-    // loop 10 times
-    // push a random number from 1-6 to my array
-    // return array
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.ceil(Math.random() * 6));
     }
     return newDice;
   }
-  console.log(allNewDice());
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-button" onClick={rollDice}>Roll</button>
     </main>
   );
 }
